958cbbcef0cb7f482d4ab371965cde279be3184b
Jackfrog1 <112097363+Jackfrog1@users.noreply.github.com>
2022-09-06 19:55:36 +0200

    Added coroutine

--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -14,12 +14,22 @@ public class Horse : MonoBehaviour
 
     public List<GameObject> allGrass = new List<GameObject>();  //Liste til alt græsset i scenen
     public State myState;                                       //enum-objekt som holder styr på hvilken state man er i
-    public GameObject bed;                                  //senge-objekt
+    //public GameObject bed;                                  //senge-objekt
     public NavMeshAgent agent;                              //navmesh-agent
+    public GameObject horseObj;
+    public int grassEaten;
+    public int foodRequiredToGiveBirth;
+    bool grown = false;
+    static int numberOfHorses;
+    public int maxHorses;
     
     void Start()
     {
-        bed = GameObject.Find("Bed");                       //Finder et gameobject i scenen der hedder "Bed" og gemmer det i bed-variablen
+        numberOfHorses++;
+        // bed = GameObject.Find("Bed");                       //Finder et gameobject i scenen der hedder "Bed" og gemmer det i bed-variablen
+        myState = State.Eat;                                    //Starter altid i Eat-state
+
+        StartCoroutine(GrowUp());
     }
     
     void Update()
@@ -33,9 +43,7 @@ public class Horse : MonoBehaviour
             case State.Eat:
                 Eat();
                 break;
-        }
-
-        
+        }  
     }
 
     void Sleep()                                            //sleep-metode
@@ -45,9 +53,6 @@ public class Horse : MonoBehaviour
 
     void Eat()                                              //Eat-metoden
     {
-        
-
-
         GameObject closestGrass;                            //Lokal gameobject-variabel til det nærmeste stykke græs
         float shortestDistance = 9999;                      //lokal float-variabel til at holde den korteste afstand til et stykke græs
 
@@ -89,10 +94,46 @@ public class Horse : MonoBehaviour
     {
         if (collision.collider.tag == "Grass")                  //Hvis det hesten rammer er tagget "Grass"
         {
+            grassEaten += 1;
+            GameObject.Find("Spawner").GetComponent<Spawner>().NumberOfGrassMinusOne();
+
+            if (grassEaten >= foodRequiredToGiveBirth && grown == true && numberOfHorses < maxHorses)     //hvis der er spist nok OG grown == true OG antal heste er under maxHorses
+            {
+                grassEaten = 0;
+                StartCoroutine(GiveBirth());                    //Starter coroutine
+            }
+
             allGrass.Remove(collision.collider.gameObject);     //Fjern det stykke græs fra listen allGrass
             Destroy(collision.collider.gameObject);             //Fjern det stykke græs helt fra spillet
         }
     }
 
+    IEnumerator GiveBirth()                                     
+    {
+        for (int i = 0; i < 80; i++)                                                    //for-loop som kører 80 gange
+        {
+            this.transform.localScale += new Vector3(0.004f,0.004f,0.004f);             //Gør hesten større
+            yield return new WaitForSeconds(0.01f);                                     //venter 0.05 sekund
+        }
 
+        this.transform.localScale = new Vector3(1,1,1);                                 //sætter hesten til almindelig størrelse
+        Instantiate(horseObj, this.transform.position, Quaternion.identity);            //spanwner en ny hest
+    }
+
+    IEnumerator GrowUp() 
+    {
+
+        this.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);                        //sætter hesten til lille størrelse
+
+        for (int i = 0; i < 350; i++)
+        {
+            this.transform.localScale += new Vector3(0.002f, 0.002f, 0.002f);             //Gør hesten større
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        this.transform.localScale = new Vector3(1, 1, 1);                                 //sætter hesten til almindelig størrelse
+        grown = true;                                                                       // bool-variabel
+    }
+
+    
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,19 +7,45 @@ public class Spawner : MonoBehaviour
     public float range;                                             //Størrelsen på området den spawner i
     public float spawnCooldown;                                     //Cooldown på spawn, hold tallet over 0
     public GameObject objectToSpawn;                                //Træk det objekt ind der skal spawnes
+    public static int numberOfGrass;                                       //Static betyder den deles af alle objekter af klassen Spawner
+    public int maxGrass;
 
     
     void Start()
     {
-        InvokeRepeating("Spawn",1.5f,spawnCooldown);                               //InvokeRepeating kalder her Spawn-metoden efter 1.5 sekund, og efterfølgende én gang i sekundet
+        InvokeRepeating("Spawn",0.1f,spawnCooldown);                               //InvokeRepeating kalder her Spawn-metoden efter 0.1 sekund, og efterfølgende én gang i sekundet
+    }
+
+    public void NumberOfGrassMinusOne() 
+    {
+        numberOfGrass--;
     }
 
     void Spawn()                                                    //Spawn-metoden som spawner et objekt i en random position
     {
-        float randomX = Random.Range(-range,range);
-        float randomY = Random.Range(-range,range);
+        print(numberOfGrass);
 
-        Instantiate(objectToSpawn,this.transform.position + new Vector3(randomX,0,randomY),Quaternion.identity);            //Spawner objektet på spanwnerens position lagt sammen med en ekstra position som indeholder en random x og y værdi
+        float randomX = Random.Range(-range,range);
+        float randomZ = Random.Range(-range,range);
+
+        RaycastHit hit;
+        Ray lookDownRay = new Ray(new Vector3(this.transform.position.x + randomX, 50f, this.transform.position.z + randomZ), Vector3.down);            //raycast
+
+        if (Physics.Raycast(lookDownRay, out hit, 100f) == true)
+        {
+            
+            if (hit.collider.tag == "Terrain")
+            {
+                if (numberOfGrass < maxGrass)
+                {
+                    Instantiate(objectToSpawn, hit.point + new Vector3(0, 0.0f, 0), Quaternion.identity);            //Spawner objektet der hvor strålen rammer
+                    numberOfGrass++;                                                                                 // +1
+                }
+              
+            }
+        }
+
+       
 
     }
 }

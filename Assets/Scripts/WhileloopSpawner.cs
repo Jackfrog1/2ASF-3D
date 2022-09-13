1a48095eadcf9fb2cb5aa33ec350257ea2d3d5d6
Jackfrog1 <112097363+Jackfrog1@users.noreply.github.com>
2022-09-13 10:25:02 +0200

    Added loop task

--- Assets/Scripts/WhileloopSpawner.cs
+++ Assets/Scripts/WhileloopSpawner.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class WhileloopSpawner : MonoBehaviour
 {
     public float range;                                             //Størrelsen på området den spawner i
     public float spawnCooldown;                                     //Cooldown på spawn, hold tallet over 0
     public GameObject objectToSpawn;                                //Træk det objekt ind der skal spawnes
     public static int numberOfGrass;                                       //Static betyder den deles af alle objekter af klassen Spawner
     public int maxGrass;
-
-    
+   
     void Start()
     {
-        InvokeRepeating("Spawn",0.1f,spawnCooldown);                               //InvokeRepeating kalder her Spawn-metoden efter 0.1 sekund, og efterfølgende hver gang der er gået SpawnCooldown sekunder
+        // OPGAVE: Lav så den spawner med While-loop
     }
 
     public void NumberOfGrassMinusOne()                                             //En metode andre klasser kalder for at sænke numberOfGrass med én
@@ -32,20 +31,15 @@ public class Spawner : MonoBehaviour
         Ray lookDownRay = new Ray(new Vector3(this.transform.position.x + randomX, 50f, this.transform.position.z + randomZ), Vector3.down);            //raycast
 
         if (Physics.Raycast(lookDownRay, out hit, 100f) == true)
-        {
-            
+        {           
             if (hit.collider.tag == "Terrain")
             {
                 if (numberOfGrass < maxGrass)
                 {
                     Instantiate(objectToSpawn, hit.point + new Vector3(0, 0.0f, 0), Quaternion.identity);            //Spawner objektet der hvor strålen rammer
                     numberOfGrass++;                                                                                 // +1
-                }
-              
+                }             
             }
         }
-
-
-       
     }
 }
